72c7fa151f0aa02297f6527deebbd644efb3c99f
Simone Brunello <92174029+SushiAndCode@users.noreply.github.com>
2025-02-12 01:01:46 +0100

    ,

--- ex9.py
+++ ex9.py
@@ -1,3 +1,18 @@
-stringa  = '('
+import pandas as pd
 
-print(stringa[::-1])
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    
+    employee.sort_values(by = 'salary', ascending = False, inplace = True, ignore_index = True)
+    employee.rename(columns = {'salary':'SecondHighestSalary'},inplace = True)
+    employee.drop_duplicates(subset=["SecondHighestSalary"],inplace = True,ignore_index=True)
+    if len(employee["SecondHighestSalary"])>1:
+        return employee.loc[[1],['SecondHighestSalary']]
+    else:
+        return pd.DataFrame([None],columns = ['SecondHighestSalary'])
+    
+df = pd.DataFrame({
+    "id": [1, 2,3],
+    "salary": [100, 100,50]
+})
+
+second_highest_salary(df)
